9f0497d10d5b4b86123f350680c424235f9dbda4
Michael <mikesilkysmooth@gmail.com>
2025-03-13 14:07:28 -0400

    fixed print script

--- script.py
+++ script.py
@@ -2,4 +2,4 @@
 print("Welcome to the Monty Python Adventure!\nYou are about to embark on a quest for the Holy Grail. Beware of dangerous encounters!")
 # this takes an input from the user and stores it in player_name and welcomes them 
 player_name = input("What is your name, brave adventurer?")
-print(f"Welcome, Sir player_name of Pythonia!")
+print(f"Welcome, Sir {player_name} of Pythonia!")
