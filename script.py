d98b7d5447b05931bec0ce88c5f8a42f30c7dce8
Michael <mikesilkysmooth@gmail.com>
2025-03-13 14:09:09 -0400

    made it so it asks for input on a new line so that it looks cleaner

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 # This is an introduction to the game
 print("Welcome to the Monty Python Adventure!\nYou are about to embark on a quest for the Holy Grail. Beware of dangerous encounters!")
 # this takes an input from the user and stores it in player_name and welcomes them 
-player_name = input("What is your name, brave adventurer?")
+player_name = input("What is your name, brave adventurer?\n")
 print(f"Welcome, Sir {player_name} of Pythonia!")
